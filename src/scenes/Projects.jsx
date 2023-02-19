59a364720f478f9d5c4a1ebb7135771116f349db
Aaron Chan <a20chan@ryerson.ca>
2023-02-19 12:53:45 -0500

    fixing projects photo area

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -27,7 +27,7 @@ const Project = ({title, subtitle}) => {
         <p className="text-2xl font-playfair">{title}</p>
         <p className="mt-7">{subtitle}</p>
       </div>
-      <img src={`../assets/${projectTitle}.JPG`} alt={projectTitle}/>
+      <img src={`assets/${projectTitle}.JPG`} alt={projectTitle}/>
     </motion.div>
   )
 }
