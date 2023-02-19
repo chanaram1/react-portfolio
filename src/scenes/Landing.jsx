41bf2fed4ec45e5962e63bca0d586c5dacd52654
Aaron Chan <a20chan@ryerson.ca>
2023-02-19 12:39:02 -0500

    fixing errors

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -22,7 +22,7 @@ const Landing = ({setSelectedPage}) => {
               alt="profile"
               className="hover:filter hover:saturate-200 transition duration-500 
               z-10 w-full max-w-[400px] md:max-w-[600px]"
-              src="assets/profile-image.jpg"
+              src="assets/profile-image.JPG"
             />
           </div>
         ) : (
@@ -30,7 +30,7 @@ const Landing = ({setSelectedPage}) => {
               alt="profile"
               className="hover:filter hover:saturate-200 transition duration-500 
               z-10 w-full max-w-[400px] md:max-w-[600px]"
-              src="assets/profile-image.jpg"
+              src="assets/profile-image.JPG"
             />
         )}
       </div>
--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -19,7 +19,7 @@ const Project = ({title, subtitle}) => {
   const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 
   transition duration-500 bg-grey z-30 flex flex-col justify-center 
   items-center text-center p-16 text-deep-blue`;
-  const projectTitle = title.split(" ").join("").toLowerCase();
+  const projectTitle = title.split(" ").join("-").toLowerCase();
   
   return(
     <motion.div variants={projectVariant} className="relative">
